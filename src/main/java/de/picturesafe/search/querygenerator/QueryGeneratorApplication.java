8df99d7e9405ae0274e39ef7f31f2cc4428dc498
Jörg Frommann <jfrommann@picturesafe.de>
2020-09-09 17:40:24 +0200

    Implemented Boost

--- src/main/java/de/picturesafe/search/querygenerator/QueryGeneratorApplication.java
+++ src/main/java/de/picturesafe/search/querygenerator/QueryGeneratorApplication.java
@@ -16,11 +16,42 @@
 
 package de.picturesafe.search.querygenerator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class QueryGeneratorApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueryGeneratorApplication.class);
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            try {
+                logBeanDefinitions(ctx);
+            } catch (Exception e) {
+                LOGGER.error("Command line runner failed!", e);
+            }
+        };
+    }
+
+    private void logBeanDefinitions(ApplicationContext ctx) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Running Spring Boot application with following bean definitions:");
+            final String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (final String beanName : beanNames) {
+                LOGGER.debug("- {} : {}", beanName, ctx.getType(beanName));
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(QueryGeneratorApplication.class, args);
